cc97c3385c71e7e4c5aaaf2114cbb55c74f3f997
Dan Parsons <dparsons@nyip.net>
2015-06-11 14:27:38 -0700

    added license

--- fetch_exceptions.py
+++ fetch_exceptions.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # by Dan Parsons, aka lerxst, <dparsons@mozilla.com>
 
 # This script does several things:
--- iplimit.py
+++ iplimit.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # by Dan Parsons, aka lerxst, <dparsons@mozilla.com>
 
 from flask import Flask
